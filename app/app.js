bf2a50834ca6a3b4527f357c87da15b625adfe79
AlexMarvelo <mironenkoalexey@gmail.com>
2016-09-26 16:33:08 +0300

    config webpack

--- app/app.js
+++ app/app.js
@@ -1,9 +1,21 @@
 'use strict';
+require('angular');
+
+// Import styles
+require('./theme/css/bootstrap.min.css');
+require('./theme/css/heroic-features.css');
+
+// Import components
+require('./components/navbar/navbar.module.js');
+require('./components/navbar/navbar.component.js');
 
 // Declare app level module which depends on views, and components
 angular.module('OMDbHero', [
   'navbar'
-]).
-config([function() {
-  
+])
+
+.config([() => {
+  //asd
 }]);
+
+angular.bootstrap(document, ['OMDbHero']);
--- app/components/navbar/navbar.component.js
+++ app/components/navbar/navbar.component.js
@@ -3,14 +3,19 @@
 angular.
   module('navbar').
   component('navbar', {
-    templateUrl: 'components/navbar/navbar.template.html',
-    controller: [
-      function NavbarController() {
+    templateUrl: 'app/components/navbar/navbar.template.html',
+    controller: ['$scope',
+      function NavbarCtrl($scope) {
+        this.homeLink = '#';
+        this.homeLinkTitle = 'OMDb Hero';
+
         // var self = this;
         //
         // $http.get('phones/' + $routeParams.phoneId + '.json').then(function(response) {
         //   self.phone = response.data;
         // });
+
+        // console.log($scope);
       }
     ]
   });
